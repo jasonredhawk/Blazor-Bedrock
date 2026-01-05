1e33340fcb4adf59ce0811669c5e92215c9ee980
Jason Thomas <jason.redhawk@gmail.com>
2026-01-05 01:49:37 -0500

    decent

--- Blazor Bedrock/obj/Debug/net9.0/Blazor Bedrock.AssemblyInfo.cs
+++ Blazor Bedrock/obj/Debug/net9.0/Blazor Bedrock.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blazor_Bedrock")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+929da7ff9e7d6c608a61b640440dba026fcbaae1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+157a799168958c15aea986dad087029c572ac88a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blazor_Bedrock")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blazor_Bedrock")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
